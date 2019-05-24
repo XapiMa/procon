1f4282efd02fb73ec3cb51ddf8188365e4eedabf
Naoki Harima <hurry4170@gmail.com>
2019-05-24 23:56:03 +0900

    add: todys ab

---
+++ contest/atcoder/abc/abc068/a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i = 0; i < (int)(n); i++)
+#define REP(i,k,n) for(int i = k; i < (int)(n); i++)
+typedef long long ll;
+
+#define vi vector<int>
+#define pb push_back
+
+int main(){
+
+    string n;
+    cin >> n;
+    cout << "ABC"+n << endl;
+    return 0;
+}
---
+++ contest/atcoder/abc/abc068/b.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i = 0; i < (int)(n); i++)
+#define REP(i,k,n) for(int i = k; i < (int)(n); i++)
+typedef long long ll;
+
+#define vi vector<int>
+#define pb push_back
+
+int main(){
+int n,ans;
+cin >> n;
+int i;
+for(i=1;i<=n;i*=2){}
+
+cout << i/2 << endl;
+    return 0;
+}
